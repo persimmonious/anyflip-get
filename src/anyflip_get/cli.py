3b55ee47e9877085da3d1a0da4ec47a5a6f15b02
persimmonious <msharipovr@gmail.com>
2026-06-16 12:23:23 -0400

    feat: basic functionality

---
+++ src/anyflip_get/cli.py
@@ -0,0 +1,28 @@
+import click
+from urllib import request
+import os
+import random
+import sys
+
+
+@click.command()
+@click.argument('source')
+@click.option('--pages',
+              type=click.INT,
+              prompt='How many pages to download',
+              help='Number of pages to download.')
+def cli(source, pages):
+    """SOURCE example: https://online.anyflip.com/xxxx/yyyy"""
+    dirname = hex(random.randint(0x100000000000, 0xffffffffffff))[2:]
+    os.mkdir(dirname)
+    dest = os.path.join(os.curdir, dirname)
+    for i in range(1, pages + 1):
+        url = f'{source}/files/mobile/{i}.jpg'
+        filename = f'{dest}/{i}.jpg'
+        request.urlretrieve(url, filename)
+        print(f'Dowloaded page {i}', file=sys.stderr)
+    print('Done!', file=sys.stderr)
+
+
+def main():
+    cli()
