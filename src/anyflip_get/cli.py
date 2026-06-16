c2a0fda82c604aad063fbb3cb3e946c630705edd
persimmonious <msharipovr@gmail.com>
2026-06-16 14:49:31 -0400

    feat: save pages to PDF

--- src/anyflip_get/cli.py
+++ src/anyflip_get/cli.py
@@ -2,20 +2,16 @@ import click
 import urllib
 from urllib import request
 import os
+import sys
+from PIL import Image
+import tempfile
 
 
-def download(source, pages, output, verbose):
-    dirname = output
-    if not dirname:
-        dirname = os.getcwd()
-    dirname = os.path.abspath(dirname)
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-
+def download_jpgs(source, pages, dest, verbose):
     downloaded = 0
     for i in range(1, pages + 1):
         url = f'{source}/files/mobile/{i}.jpg'
-        filename = os.path.join(dirname, f'{i}.jpg')
+        filename = os.path.join(dest, f'{i}.jpg')
         try:
             request.urlretrieve(url, filename)
         except urllib.error.URLError as e:
@@ -24,10 +20,44 @@ def download(source, pages, output, verbose):
             raise e
         else:
             downloaded += 1
-        if verbose:
-            click.echo(f'Dowloaded page {i}', err=True)
+            if verbose:
+                click.echo(f'Dowloaded page {i}.', err=True)
+
+    return downloaded
+
+
+def create_pdf(tmp_dir, pages, pdf_dest):
+    paths = (os.path.join(tmp_dir, f'{i}.jpg') for i in range(1, pages + 1))
+    jpegs = [Image.open(p).convert("RGB") for p in paths]
+    jpegs[0].save(pdf_dest, "PDF", save_all=True, append_images=jpegs[1:])
+
+
+def download_as_pages(source, pages, output, verbose):
+    dest = output
+    if not dest:
+        dest = os.getcwd()
+    dest = os.path.abspath(dest)
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    downloaded = download_jpgs(source, pages, dest, verbose)
+    failed = pages - downloaded
     if verbose:
-        click.echo(f'{downloaded} pages saved to {dirname}', err=True)
+        click.echo(f'{downloaded} pages saved to {dest}.', err=True)
+        if failed > 0:
+            click.echo(f'{failed} pages failed to download.', err=True)
+
+
+def download_to_pdf(source, pages, pdf_dest, verbose):
+    pdf_dest = os.path.abspath(pdf_dest)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        downloaded = download_jpgs(source, pages, tmp_dir, verbose)
+        failed = pages - downloaded
+        if failed > 0:
+            click.echo('Download incomplete! PDF creation aborted.', err=True)
+            sys.exit(1)
+        create_pdf(tmp_dir, pages, pdf_dest)
+        if verbose:
+            click.echo(f'{pdf_dest} created from {pages} pages.', err=True)
 
 
 @click.command()
@@ -42,10 +72,21 @@ def download(source, pages, output, verbose):
 @click.option('--verbose', '-v',
               is_flag=True,
               help='Verbose output')
+@click.option('--pdf', '-P',
+              is_flag=True,
+              help='Combine the downloaded pages into a PDF')
 @click.version_option()
-def cli(source, pages, output, verbose):
+def cli(source, pages, output, verbose, pdf):
     """SOURCE example: https://online.anyflip.com/xxxx/yyyy"""
-    download(source, pages, output, verbose)
+    if pdf and not output:
+        click.echo(
+            "Error! File name must be specified when using --pdf flag.",
+            err=True)
+        sys.exit(1)
+    if pdf:
+        download_to_pdf(source, pages, output, verbose)
+    else:
+        download_as_pages(source, pages, output, verbose)
 
 
 def main():
