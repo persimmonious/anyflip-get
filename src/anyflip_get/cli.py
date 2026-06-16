5ff980928dabde1ca7106637cd8a85bcb885b57f
persimmonious <msharipovr@gmail.com>
2026-06-16 13:15:24 -0400

    feat: change download directory

--- src/anyflip_get/cli.py
+++ src/anyflip_get/cli.py
@@ -1,7 +1,7 @@
 import click
+import urllib
 from urllib import request
 import os
-import random
 
 
 @click.command()
@@ -10,17 +10,32 @@ import random
               type=click.INT,
               prompt='How many pages to download',
               help='Number of pages to download.')
-def cli(source, pages):
+@click.option('--output',
+              type=click.Path(writable=True),
+              help='Where to save the downloaded files')
+def cli(source, pages, output):
     """SOURCE example: https://online.anyflip.com/xxxx/yyyy"""
-    dirname = hex(random.randint(0x100000000000, 0xffffffffffff))[2:]
-    os.mkdir(dirname)
-    dest = os.path.join(os.curdir, dirname)
+    dirname = output
+    if not dirname:
+        dirname = os.getcwd()
+    dirname = os.path.abspath(dirname)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    downloaded = 0
     for i in range(1, pages + 1):
         url = f'{source}/files/mobile/{i}.jpg'
-        filename = f'{dest}/{i}.jpg'
-        request.urlretrieve(url, filename)
+        filename = os.path.join(dirname, f'{i}.jpg')
+        try:
+            request.urlretrieve(url, filename)
+        except urllib.error.URLError as e:
+            raise e
+        except urllib.error.ContentTooShortError as e:
+            raise e
+        else:
+            downloaded += 1
         click.echo(f'Dowloaded page {i}', err=True)
-    click.echo('Done!', err=True)
+    click.echo(f'{downloaded} pages saved to {dirname}', err=True)
 
 
 def main():
